f694368c51278df0384f9afff20cf8838b77712b
sRamiro <ramiro_sando@outlook.com>
2020-04-22 10:49:26 -0300

    h1 added

---
+++ script/addElements.js
@@ -0,0 +1,6 @@
+const mainContainer = document.querySelector('.mainContent');
+
+const h1Element = document.createElement('h1');
+h1Element.textContent = 'Hello world!';
+
+mainContainer.appendChild(h1Element); 
